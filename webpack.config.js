6050eb614f1e671eca5b4ef7a851ab4a24cec2d4
Mathieul <mathieul@pop-os.localdomain>
2020-10-16 05:47:55 +0200

    chore(my-react): refactoring the file

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 const devMode = process.env.NODE_ENV !== 'production'
+
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
   template: './src/index.html',
   filename: './index.html'
 })
+
 const miniCssExtractPlugin = new MiniCssExtractPlugin({
   filename: '[name].css',
   chunkFilename: '[id].css'
 })
+
 module.exports = {
   module: {
     rules: [
